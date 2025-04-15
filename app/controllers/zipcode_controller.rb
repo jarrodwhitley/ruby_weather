9a2d384fb4520f9bf216bd7564ddae12aa1cfc28
Jarrod Whitley <jarrodwhitley@gmail.com>
2025-04-15 14:14:25 -0500

    everything works!

---
+++ app/controllers/zipcode_controller.rb
@@ -0,0 +1,44 @@
+class ZipcodeController < ApplicationController
+  def index
+    @zip_query = params[:zipcode]
+    if @zip_query.blank?
+      flash[:alert] = "You must enter a zipcode"
+      redirect_to root_path
+    else
+      require "net/http"
+      require "json"
+
+      @url = "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=#{@zip_query}&distance=25&API_KEY=17327A63-EDE4-40C1-981F-B9C56BE8C55B"
+      @uri = URI(@url)
+      @response = Net::HTTP.get(@uri)
+
+      # debug @response
+      puts "RESPONSE: #{@response}"
+
+      @output = JSON.parse(@response)
+
+      if @output.empty? || !@output
+        flash[:alert] = "Error fetching data for the provided zipcode."
+        redirect_to root_path
+      else
+        @final_output = @output[0]
+        @aqi_value = @final_output["AQI"]
+        @aqi_color = setAqiColor(@aqi_value)
+        @aqi_description = @final_output["Category"]["Name"]
+      end
+    end
+  end
+
+  private
+
+  def setAqiColor(num)
+    case num
+    when 0..50 then "green"
+    when 51..100 then "yellow"
+    when 101..150 then "orange"
+    when 151..200 then "red"
+    when 201..300 then "purple"
+    else "maroon"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post "/zipcode" => "home#zipCode", as: "zipcode"
-  # get "/home/zipcode" => "home#zipcode", as: "home_zipcode"
+  post "/zipcode" => "zipcode#index", as: "zipcode"
+  #   get "/zipcode" => "zipcode#index"
   root "home#index"
 end
